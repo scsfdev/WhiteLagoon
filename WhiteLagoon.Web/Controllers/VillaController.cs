c1ccd66a3e173c4a533ef6d8a3f9c2cd379df8e8
Tin <25923486+scsfdev@users.noreply.github.com>
2024-08-23 13:35:03 +0800

    Create, Update Finish. Now in Delete.

--- WhiteLagoon.Web/Controllers/VillaController.cs
+++ WhiteLagoon.Web/Controllers/VillaController.cs
@@ -30,11 +30,69 @@ namespace WhiteLagoon.Web.Controllers
         {
             if(obj.Name == obj.Description)
             {
-                ModelState.AddModelError("", "The desc cannot match wtih Name.");
+                ModelState.AddModelError("Name", "The desc cannot match wtih Name.");
             }
             if(ModelState.IsValid)
             {
                 _db.Villas.Add(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+        }
+
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(v => v.Id == villaId);
+
+            // Filtering result.
+            //var objList = _db.Villas.Where(v => v.Price > 40 && v.Occupancy > 2);
+            //var obj2 = _db.Villas.Find(villaId);
+            //Villa? obj3 = _db.Villas.FirstOrDefault(v => v.Price > 40 && v.Occupancy > 2);
+
+            if(obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index", "Villa");
+            }
+            return View();
+        }
+
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(v => v.Id == villaId);
+            if (obj == null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Remove(obj);
                 _db.SaveChanges();
 
                 return RedirectToAction("Index", "Villa");
