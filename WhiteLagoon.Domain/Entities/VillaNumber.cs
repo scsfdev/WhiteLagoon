77064968c251bc9d01bd00f10b49616205976c67
Tin <25923486+scsfdev@users.noreply.github.com>
2024-08-27 14:05:42 +0800

    Villa Number under development.

--- WhiteLagoon.Domain/Entities/VillaNumber.cs
+++ WhiteLagoon.Domain/Entities/VillaNumber.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -11,10 +12,14 @@ namespace WhiteLagoon.Domain.Entities
     public class VillaNumber
     {
         [Key, DatabaseGenerated(DatabaseGeneratedOption.None)]
+        [Display(Name ="Villa Number")]
         public int Villa_Number { get; set; }
 
         [ForeignKey("Villa")]
         public int VillaId { get; set; }
+
+        // Another way to Skip below Model to exclude from ModelStae Validation.
+        [ValidateNever]
         public Villa Villa { get; set; }
 
         public string? SpecialDetails { get; set; }
---
+++ WhiteLagoon.Web/Controllers/VillaNumberController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WhiteLagoon.Domain.Entities;
+using WhiteLagoon.Infrastructure.Data;
+
+namespace WhiteLagoon.Web.Controllers
+{
+
+    public class VillaNumberController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public VillaNumberController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var villaNumbers = _db.VillaNumbers.ToList();
+            return View(villaNumbers);
+        }
+
+        public IActionResult Create()
+        {
+            IEnumerable<SelectListItem> list = _db.Villas.ToList().Select(v => new SelectListItem
+            {
+                Text = v.Name,
+                Value= v.Id.ToString()
+            });
+            ViewData["VillaList"] = list;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(VillaNumber obj) 
+        {
+            // One way to remove model validation.
+            //ModelState.Remove("Villa");
+            // The other way is to change from Model (Entities) >> by adding [ValidateNever] tag but need to adjust Project File with ItemGroup --> <FrameworkReference Include="Microsoft.AspNetCore.App" />
+
+            if (ModelState.IsValid)
+            {
+                _db.VillaNumbers.Add(obj);
+                _db.SaveChanges();
+
+                TempData["success"] = "The villa number has been created successfully.";
+                return RedirectToAction("Index", "VillaNumber");
+            }
+            return View();
+        }
+
+
+        public IActionResult Update(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(v => v.Id == villaId);
+
+            if(obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        public IActionResult Update(Villa obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Villas.Update(obj);
+                _db.SaveChanges();
+
+                TempData["success"] = "The villa number has been updated successfully.";
+                return RedirectToAction("Index", "VillaNumber");
+            }
+            return View();
+        }
+
+
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(v => v.Id == villaId);
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+
+            return View(obj);
+        }
+
+
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(v => v.Id == obj.Id);
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+
+                TempData["success"] = "The villa number has been deleted successfully.";
+                return RedirectToAction("Index", "VillaNumber");
+            }
+            TempData["error"] = "The villa number could not be deleted.";
+            return View();
+        }
+    }
+}
